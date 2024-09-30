680a9cae0062bc292b4462f8e9af8a036b09968f
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2024-09-30 11:51:02 +0530

    changes

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# String Concatenation
+# youtuber = 'FCC'
+# few ways to concatenate
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+# Using madlibs
+madlib = f"Computer programming is {adj} . It makes me excited \
+I love programming"
+print(madlib)
