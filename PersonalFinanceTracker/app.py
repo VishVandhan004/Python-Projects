8a4e19911a1a0ecf95d29558d8b1d64f0c487f19
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-05-01 23:51:07 +0530

    changes

--- PersonalFinanceTracker/app.py
+++ PersonalFinanceTracker/app.py
@@ -57,9 +57,9 @@ def init_db():
         cur.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
-                username VARCHAR(50) UNIQUE NOT NULL,
-                email VARCHAR(100) UNIQUE NOT NULL,
-                password VARCHAR(100) NOT NULL
+                username VARCHAR(100) UNIQUE NOT NULL,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL
             )
         ''')
         
