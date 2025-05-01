e35e72423f9f408c213f45a661cd090d9e54f64c
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-05-01 22:31:39 +0530

    changes

--- PersonalFinanceTracker/app.py
+++ PersonalFinanceTracker/app.py
@@ -288,5 +288,7 @@ def stats():
         cur.close()
         conn.close()
 
+# Add at the bottom
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
