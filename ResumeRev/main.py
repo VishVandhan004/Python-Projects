43d56aa909d9c6b55568e9944c17bbdb2bf0f62f
Vishnu Vandhan Talakokkula <talakokkula.vishnu2004@gmail.com>
2025-05-14 08:13:17 +0530

    changes

--- ResumeRev/main.py
+++ ResumeRev/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from resumerev!")
+from dotenv import load_dotenv
+load_dotenv()
 
+import streamlit as st
+import os
+from PIL import Image
+import pdf2image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 if __name__ == "__main__":
     main()
